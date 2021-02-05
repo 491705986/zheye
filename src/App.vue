6991d8a299d4cc72db62c2e9267443765f613562
Ben <491705986@qq.com>
2021-02-06 00:26:55 +0800

    feat: 提交column组件

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <ColumnList :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: '123',
+    description: '奥术大师大多',
+    avatar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F07%2F20200607222012_lwusd.thumb.400_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615134040&t=2b07af99f5cebf9d6d12d2f22e8d351e'
+  },
+  {
+    id: 2,
+    title: '1234',
+    description: '奥术大师大多2',
+    avatar:
+      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F07%2F20200607222012_lwusd.thumb.400_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615134040&t=2b07af99f5cebf9d6d12d2f22e8d351e'
+  }
+];
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld
+  name: 'App',
+  components: { ColumnList },
+  setup() {
+    return {
+      list: testData
+    };
   }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
