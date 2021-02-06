6e157c310e1d2b1c0de6eb27369cb315f142f552
yanbenyuan <491705986@qq.com>
2021-02-06 10:45:41 +0800

    feat: 新增ColumnList组件

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+import Logo from './assets/logo.png';
 
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: '123',
-    description: '奥术大师大多',
-    avatar:
-      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F07%2F20200607222012_lwusd.thumb.400_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615134040&t=2b07af99f5cebf9d6d12d2f22e8d351e'
-  },
-  {
-    id: 2,
-    title: '1234',
-    description: '奥术大师大多2',
-    avatar:
-      'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fc-ssl.duitang.com%2Fuploads%2Fitem%2F202006%2F07%2F20200607222012_lwusd.thumb.400_0.jpeg&refer=http%3A%2F%2Fc-ssl.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1615134040&t=2b07af99f5cebf9d6d12d2f22e8d351e'
+    description:
+      "Some quick example text to build on the card title and make up the bulk of the card's content."
+    // avatar: Logo
   }
 ];
 
@@ -30,8 +24,12 @@ export default defineComponent({
   name: 'App',
   components: { ColumnList },
   setup() {
+    const list = ref<ColumnProps[]>([]);
+    list.value = new Array(9)
+      .fill(testData[0])
+      .map((item, index) => ({ ...item, id: index + 1 }));
     return {
-      list: testData
+      list
     };
   }
 });
