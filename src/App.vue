149d265120e9b63a1f41bb10c8c6a4f1ce3c1e5c
yanbenyuan <491705986@qq.com>
2021-02-06 11:55:51 +0800

    feat: 新增nav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container">
+    <GlobalHeader :user="currentUser" />
     <ColumnList :list="list" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import ColumnList, { ColumnProps } from './components/ColumnList.vue';
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue';
+import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue';
 import Logo from './assets/logo.png';
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: 'Ben',
+  id: '123'
+};
 
 const testData: ColumnProps[] = [
   {
@@ -22,14 +29,15 @@ const testData: ColumnProps[] = [
 
 export default defineComponent({
   name: 'App',
-  components: { ColumnList },
+  components: { ColumnList, GlobalHeader },
   setup() {
     const list = ref<ColumnProps[]>([]);
     list.value = new Array(9)
       .fill(testData[0])
       .map((item, index) => ({ ...item, id: index + 1 }));
     return {
-      list
+      list,
+      currentUser
     };
   }
 });
