53a947b9abda8e34cc1bcda9b51698ade2c8af60
yanbenyuan <491705986@qq.com>
2021-02-06 17:45:55 +0800

    feat: validateInput组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser" />
-    <ColumnList :list="list" />
+    <!--    <ColumnList :list="list" />-->
+    <form>
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <ValidateInput v-model="emailRef.val" :rules="emailRules" />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, reactive } from 'vue';
 import ColumnList, { ColumnProps } from '@/components/ColumnList.vue';
 import GlobalHeader, { UserProps } from '@/components/GlobalHeader.vue';
+import ValidateInput, { RulesProp } from '@/components/ValidateInput.vue';
 import Logo from './assets/logo.png';
 
 const currentUser: UserProps = {
@@ -27,17 +34,42 @@ const testData: ColumnProps[] = [
   }
 ];
 
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
 export default defineComponent({
   name: 'App',
-  components: { ColumnList, GlobalHeader },
+  components: { ColumnList, GlobalHeader, ValidateInput },
   setup() {
     const list = ref<ColumnProps[]>([]);
     list.value = new Array(9)
       .fill(testData[0])
       .map((item, index) => ({ ...item, id: index + 1 }));
+
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ];
+
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    });
+    const validateEmail = () => {
+      if (emailRef.val === '') {
+        emailRef.error = true;
+        emailRef.message = 'can not be empty';
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true;
+        emailRef.message = 'should be valid email';
+      }
+    };
     return {
       list,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail,
+      emailRules
     };
   }
 });
