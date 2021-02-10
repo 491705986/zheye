dc996c3d2e4faf8bf3cf681caf7d7c1adb48d511
Ben <491705986@qq.com>
2021-02-10 18:24:02 +0800

    feat: 路由鉴权

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import store from '@/store';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 import ColumnDetail from '@/views/ColumnDetail.vue';
@@ -9,10 +10,37 @@ const router = createRouter({
   history: routerHistory,
   routes: [
     { path: '/', name: 'home', component: Home },
-    { path: '/login', name: 'login', component: Login },
-    { path: '/column/:id', name: 'column', component: ColumnDetail },
-    { path: '/create', name: 'create', component: CreatePost }
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        redirectAlreadyLogin: true
+      }
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreatePost,
+      meta: { requiredLogin: true }
+    },
+    {
+      path: '/column/:id',
+      name: 'column',
+      component: ColumnDetail,
+      meta: { requiredLogin: true }
+    }
   ]
+});
+
+router.beforeEach((to, form, next) => {
+  if (to.meta.requiredLogin && !store.state.user.isLogin) {
+    next({ name: 'login' });
+  } else if (to.meta.redirectAlreadyLogin && store.state.user.isLogin) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
